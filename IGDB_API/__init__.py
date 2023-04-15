9e795d6af1310e87c0b47ef1bf76c631c0d5f7eb
Harsh Navdhare <harsh.navdhare@proton.me>
2023-04-15 20:04:55 +0530

    Fix binding to all interfaces with hardcoded values

--- IGDB_API/__init__.py
+++ IGDB_API/__init__.py
@@ -8,4 +8,4 @@ CLIENT_ID = getenv("CLIENT_ID")
 CLIENT_SECRET = getenv("CLIENT_SECRET")
 DB_URI = getenv("DB_URI")
 PORT = int(getenv("PORT", "8000"))
-HOST = getenv("HOST", "0.0.0.0")
+HOST = getenv("HOST", "127.0.0.1")  # Use localhost locally
